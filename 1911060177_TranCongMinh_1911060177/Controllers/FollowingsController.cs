abf16f5d873cc1a25e84747185de521079d00903
Minh Trần <tcminh1108@gmail.com>
2022-03-28 00:37:47 +0700

    commit chức năng thêm change password 00:37 28/03

--- 1911060177_TranCongMinh_1911060177/Controllers/FollowingsController.cs
+++ 1911060177_TranCongMinh_1911060177/Controllers/FollowingsController.cs
@@ -21,17 +21,6 @@ namespace _1911060177_TranCongMinh_1911060177.Controllers
         [HttpPost]
         public IHttpActionResult Follow(FollowingDto followingDto)
         {
-            /*var userId = User.Identity.GetUserId();
-            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
-                return BadRequest("Following already exists!");
-            var folowing = new Following
-            {
-                FollowerId = userId,
-                FolloweeId = followingDto.FolloweeId
-            };
-            _dbContext.Followings.Add(folowing);
-            _dbContext.SaveChanges();
-            return Ok();*/
             var userId = User.Identity.GetUserId();
             var following = new Following
             {
